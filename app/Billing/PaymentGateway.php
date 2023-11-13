5023cc1d77a7f6fd341e343e2eae3ed10925ed47
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-11-13 19:20:36 +0530

    make PaymentGateway a singleton in appServiceProvider to add a discount

--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -3,16 +3,25 @@ namespace App\Billing;
 use Illuminate\Support\Str;
 
 class PaymentGateway {
+    
     private $currency;
+    private $discount;
+    
     public function __construct($currency) {
         $this->currency = $currency;
+        $this->discount = 0;
+    }
+
+    public function setDiscount($discount) {
+        $this->discount = $discount;
     }
 
     public function charge($amount) {
         return [
-            'amoun' => $amount,
+            'amoun' => $amount - $this->discount,
             'confirmation_number' => Str::random(),
-            'currency' => $this->currency
+            'currency' => $this->currency,
+            'discount' => $this->discount
         ];
     }
 }
--- app/Http/Controllers/PayOrderController.php
+++ app/Http/Controllers/PayOrderController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Billing\PaymentGateway;
+use App\Orders\OrderDetails;
 
 class PayOrderController extends Controller
 {
-    public function store(PaymentGateway $paymentGateway) {
+    public function store(OrderDetails $orderDetails, PaymentGateway $paymentGateway) {
+        $allOrderDetails = $orderDetails->all();
         dd($paymentGateway->charge(2500));
     }
 }
